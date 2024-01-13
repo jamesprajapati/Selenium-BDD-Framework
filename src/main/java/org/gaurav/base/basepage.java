883e9914a7d8cfefac3d77cba8c7591ffaae5468
Gaurav prajapati <g.m.prajapati9827@gmail.com>
2024-01-14 03:56:39 +0530

    added ci-cd file for github

--- src/main/java/org/gaurav/base/basepage.java
+++ src/main/java/org/gaurav/base/basepage.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 import java.io.File;
 import java.io.IOException;
@@ -18,13 +19,23 @@ public class basepage {
 
     public WebDriver initialize_driver() {
 
+        // Set up WebDriver and enable headless mode
         WebDriverManager.firefoxdriver().setup();
-        driver = new FirefoxDriver();
+        FirefoxOptions options = new FirefoxOptions();
+        options.setHeadless(true);
+
+        // Create the driver instance
+        driver = new FirefoxDriver(options);
+
+        // Other driver configurations
         driver.manage().deleteAllCookies();
-        driver.manage().window().fullscreen();
+        // Maximize window (instead of fullscreen, which is not applicable in headless mode)
+        driver.manage().window().maximize();
+
         tdriver.set(driver);
         return getDriver();
     }
+
     public static synchronized WebDriver getDriver() {
         return tdriver.get();
     }
